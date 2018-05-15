ebd22136472d08081685536f114855f6d91f0ab5
SzuYuan Huang <SzuYuan Huang@SZUYUANHUANG-PC>
2018-05-15 13:57:53 +0800

    Procedure

---
+++ CommerceTest/ProcedureTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Procedure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Procedure.Tests
+{
+    [TestClass()]
+    public class ProcedureTests
+    {
+        [TestMethod()]
+        public void GetBuyerOrderTest()
+        {
+            var procrdure = new Procedure();
+            var getBuyerOrder = procrdure.GetBuyerOrder(1);
+            Assert.IsTrue(getBuyerOrder.Count() != 0);
+        }
+    }
+}
---
+++ Procedure/Procedure.cs
@@ -0,0 +1,43 @@
+﻿using BuildSchool.MvcSolution.OnlineStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utils;
+
+namespace Procedure
+{
+    public class Procedure
+    {
+        public IEnumerable<Orders> GetBuyerOrder(int memberID)
+        {
+            SqlConnection connection = new SqlConnection("data source=.; database=Commerce; integrated security=true");
+            SqlCommand command = new SqlCommand();
+            SqlDataReader reader;
+
+            command.CommandText = "dbo.GetBuyerOrder";
+            command.CommandType = CommandType.StoredProcedure;
+            command.Connection = connection;
+            command.Parameters.Add(new SqlParameter("@memberID",memberID));
+
+            connection.Open();
+
+            reader = command.ExecuteReader();
+            var properties = typeof(Orders).GetProperties();
+            var Orders = new List<Orders>();
+            while (reader.Read())
+            {
+                var Order = new Orders();
+                Order = DbReaderModelBinder<Orders>.Bind(reader);
+                Orders.Add(Order);
+            }
+
+            connection.Close();
+
+            return Orders;
+        }
+    }
+}
