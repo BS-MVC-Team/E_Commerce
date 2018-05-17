3f87898efcc67ea0342de83a89d88d9c7802a626
SzuYuan Huang <SzuYuan Huang@SZUYUANHUANG-PC>
2018-05-17 18:21:23 +0800

    ProcedureAll & ProcedureUnitAll (Done)

--- CommerceTest/ProcedureTests.cs
+++ CommerceTest/ProcedureTests.cs
@@ -14,9 +14,41 @@ namespace Procedure.Tests
         [TestMethod()]
         public void GetBuyerOrderTest()
         {
-            var procrdure = new Procedure();
-            var getBuyerOrder = procrdure.GetBuyerOrder("1");
+            var procedure = new Procedure();
+            var getBuyerOrder = procedure.GetBuyerOrder("1");
             Assert.IsTrue(getBuyerOrder.Count() != 0);
+        }
+
+        [TestMethod()]
+        public void FindOrderdetaiByOrderIDTest()
+        {
+            var procedure = new Procedure();
+            var findOrderdetaiByOrderID = procedure.FindOrderdetaiByOrderID(1);
+            Assert.IsTrue(findOrderdetaiByOrderID.Count() != 0);
+        }
+
+        [TestMethod()]
+        public void GetHowLongHireDateTest()
+        {
+            var procedure = new Procedure();
+            var getHowLongHireDate = procedure.GetHowLongHireDate();
+            Assert.IsTrue(getHowLongHireDate.Count() != 0);
+        }
+
+        [TestMethod()]
+        public void FindProductsByCategoryTest()
+        {
+            var procedure = new Procedure();
+            var findProductsByCategory = procedure.FindProductsByCategory();
+            Assert.IsTrue(findProductsByCategory.Count() != 0);
+        }
+
+        [TestMethod()]
+        public void GetProductOrderTest()
+        {
+            var procedure = new Procedure();
+            var getProductOrder = procedure.GetProductOrder();
+            Assert.IsTrue(getProductOrder.Count() != 0);
         }
     }
 }
--- Procedure/Procedure.cs
+++ Procedure/Procedure.cs
@@ -10,34 +10,146 @@ using Utils;
 
 namespace Procedure
 {
+    public class GetBuyerOrderModel
+    {       
+        public string MemberID { get; set; }
+        public int OrderID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string Status { get; set; }
+    }
+
+    public class FindOrderdetaiByOrderIDModel
+    {
+        public int OrderID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string ProductName { get; set; }
+        public string Size { get; set; }
+        public string Color { get; set; }
+        public int Quantity { get; set; }
+        public string Status { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+
+    public class GetHowLongHireDateModel
+    {
+        public int EmployeeID { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public DateTime HireDate { get; set; }
+        public int How_Long { get; set; }
+    }
+
+    public class FindProductsByCategoryModel
+    {
+        public int CategroyID { get; set; }
+        public string CategoryName { get; set; }
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public string ProductImage { get; set; }
+    }
+
+    public class GetProductOrderModel
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int Total { get; set; }
+    }
+
     public class Procedure
     {
-        public IEnumerable<Orders> GetBuyerOrder(string memberID)
+        public IEnumerable<GetBuyerOrderModel> GetBuyerOrder(string memberID)
+        {
+            var command = Command("dbo.GetBuyerOrder");
+            command.Parameters.Add(new SqlParameter("@memberID", memberID));
+            command.Connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            var GetBuyerOrders = new List<GetBuyerOrderModel>();
+            while (reader.Read())
+            {
+                var GetBuyerOrder = new GetBuyerOrderModel();
+                GetBuyerOrder = DbReaderModelBinder<GetBuyerOrderModel>.Bind(reader);
+                GetBuyerOrders.Add(GetBuyerOrder);
+            }
+            command.Connection.Close();
+            return GetBuyerOrders;
+        }
+
+        public IEnumerable<FindOrderdetaiByOrderIDModel> FindOrderdetaiByOrderID(int orderid)
+        {
+            var command = Command("dbo.FindOrderdetaiByOrderID");
+            command.Parameters.Add(new SqlParameter("@orderid", orderid));
+            command.Connection.Open();
+            SqlDataReader reader = command.ExecuteReader(); 
+            var FindOrderdetaiByOrderIDs = new List<FindOrderdetaiByOrderIDModel>();
+            while (reader.Read())
+            {
+                var FindOrderdetaiByOrderID = new FindOrderdetaiByOrderIDModel();
+                FindOrderdetaiByOrderID = DbReaderModelBinder<FindOrderdetaiByOrderIDModel>.Bind(reader);
+                FindOrderdetaiByOrderIDs.Add(FindOrderdetaiByOrderID);
+            }
+            command.Connection.Close();
+            return FindOrderdetaiByOrderIDs;
+        }
+
+        public IEnumerable<GetHowLongHireDateModel> GetHowLongHireDate()
+        {
+            var command = Command("dbo.GetHowLongHireDate");
+            command.Connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            var GetHowLongHireDates = new List<GetHowLongHireDateModel>();
+            while (reader.Read())
+            {
+                var GetHowLongHireDate = new GetHowLongHireDateModel();
+                GetHowLongHireDate = DbReaderModelBinder<GetHowLongHireDateModel>.Bind(reader);
+                GetHowLongHireDates.Add(GetHowLongHireDate);
+            }
+            command.Connection.Close();
+            return GetHowLongHireDates;
+        }
+
+        public IEnumerable<FindProductsByCategoryModel> FindProductsByCategory()
+        {
+            var command = Command("dbo.FindProductsByCategory");
+            command.Connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            var FindProductsByCategories = new List<FindProductsByCategoryModel>();
+            while (reader.Read())
+            {
+                var FindProductsByCategory = new FindProductsByCategoryModel();
+                FindProductsByCategory = DbReaderModelBinder<FindProductsByCategoryModel>.Bind(reader);
+                FindProductsByCategories.Add(FindProductsByCategory);
+            }
+            command.Connection.Close();
+            return FindProductsByCategories;
+        }
+
+        public IEnumerable<GetProductOrderModel> GetProductOrder()
+        {
+            var command = Command("dbo.GetProductOrder");
+            command.Connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            var GetProductOrders = new List<GetProductOrderModel>();
+            while (reader.Read())
+            {
+                var GetProductOrder = new GetProductOrderModel();
+                GetProductOrder = DbReaderModelBinder<GetProductOrderModel>.Bind(reader);
+                GetProductOrders.Add(GetProductOrder);
+            }
+            command.Connection.Close();
+            return GetProductOrders;
+        }
+
+        public SqlCommand Command(string CommandText)
         {
             SqlConnection connection = new SqlConnection("data source=.; database=Commerce; integrated security=true");
             SqlCommand command = new SqlCommand();
-            SqlDataReader reader;
 
-            command.CommandText = "dbo.GetBuyerOrder";
+            command.CommandText = CommandText;
             command.CommandType = CommandType.StoredProcedure;
             command.Connection = connection;
-            command.Parameters.Add(new SqlParameter("@memberID",memberID));
 
-            connection.Open();
-
-            reader = command.ExecuteReader();
-            var properties = typeof(Orders).GetProperties();
-            var Orders = new List<Orders>();
-            while (reader.Read())
-            {
-                var Order = new Orders();
-                Order = DbReaderModelBinder<Orders>.Bind(reader);
-                Orders.Add(Order);
-            }
-
-            connection.Close();
-
-            return Orders;
+            return command;
         }
     }
 }
