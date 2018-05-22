f95e5175cd1d62d62e592c8b263be1fad7d56451
SzuYuan Huang <SzuYuan Huang@SZUYUANHUANG-PC>
2018-05-22 17:17:34 +0800

    MVC

---
+++ Commerce/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Commerce.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "首頁";
+
+            return View();
+        }
+
+        public ActionResult Login_Member()
+        {
+            ViewBag.Title = "登入";
+
+            return View();
+        }
+
+        public ActionResult Home()
+        {
+            ViewBag.Title = "首頁";
+
+            return View();
+        }
+
+        public ActionResult SignUp()
+        {
+            ViewBag.Title = "註冊會員";
+
+            return View();
+        }
+
+        public ActionResult Category()
+        {
+            ViewBag.Title = "目錄";
+
+            return View();
+        }
+
+        public ActionResult PageModel()
+        {
+            ViewBag.Title = "型號";
+
+            return View();
+        }
+
+        public ActionResult Login_Employee()
+        {
+            ViewBag.Title = "系統管理員登入";
+
+            return View();
+        }
+    }
+}
